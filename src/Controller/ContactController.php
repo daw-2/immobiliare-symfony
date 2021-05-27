6231f301b5c4daa5fe931d75eb75ea101c631ddf
Matthieu Mota <matthieumota@gmail.com>
2021-05-27 09:53:37 +0200

    Bonus email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,6 +7,8 @@ use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -14,7 +16,7 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -26,6 +28,19 @@ class ContactController extends AbstractController
             $em->flush();
 
             $this->addFlash('success', 'Votre message a été envoyé');
+
+            $email = (new Email())
+                ->from($contact->getEmail())
+                ->to('hello@immobiliare.com')
+                ->subject('[Demande] Un nouveau contact')
+                ->html('
+                    On a une nouvelle demande de contact <br /> <br />
+                    - Email: '.$contact->getEmail().' <br />
+                    - Nom: '.$contact->getName().' <br />
+                    - Message: '.$contact->getMessage().'
+                ');
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('contact');
         }
